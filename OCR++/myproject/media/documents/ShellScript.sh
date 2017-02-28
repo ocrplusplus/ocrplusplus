59a9f8dd1d20789da8bde65dd44563ac766561d6
Barnopriyo Barua <barno0695@gmail.com>
2017-03-01 01:28:56 +0530

    Download file not found error fixed

--- OCR++/myproject/media/documents/ShellScript.sh
+++ OCR++/myproject/media/documents/ShellScript.sh
@@ -1,3 +1,6 @@
 directory="/var/www/html/OCR++/myproject/media/documents/"
 source ~/virtual_env/v1/bin/activate
 python $directory/main_script_batch.py
+cp $directory/eval_* /var/www/html/media/documents/
+cp $directory/*.xml /var/www/html/media/documents/
+
